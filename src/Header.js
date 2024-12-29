5565a3d38fb2d727c61321bba8bead4f9745786a
drishyaa1 <drishyabhattarai@gmail.com>
2024-12-29 11:25:10 +0545

    tailwind and header started

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import logo from "./assets/aboutme.jpg";
+
 function Header(){
     console.log("Header LOaded");
     return (
-        <h1>Boop</h1>
+        <>
+        <div className="Header hidden md:flex bg-amber-300">
+            hiiiiiiiiiiii heheheh
+            <button onClick={Header}>check check</button>
+        </div>
+        <nav>
+            <div>
+                <img src={logo} alt="" width="10%"/>
+            </div>
+            <Link to="/contact">Contact Us</Link>
+            <Link to="/aboutus">About</Link>
+        </nav>
+        </>
     );
 }
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,9 +4,15 @@ import Footer from "../../Footer";
 function Layout({children}){
     return(
         <div className="container">
+            <header>
             <Header/>
+            </header>
+            <main>
             {children}
+            </main>
+            <footer>
             <Footer/>
+            </footer>
         </div>
     );
 }
