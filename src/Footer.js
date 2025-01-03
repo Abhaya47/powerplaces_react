fa85100efd526eae4524c03f08c49fd96267fcd0
Abhaya <abhay.punkid@gmail.com>
2025-01-03 13:39:29 +0545

    footer form nearly done

--- src/Footer.js
+++ src/Footer.js
@@ -4,22 +4,23 @@ function Footer(){
     return(
         <>
         <footer className="mt-10">
-            <div className="container flex md:flex-row letter mb-4  pb-4 bg-rose-200 text-yellow-100">
-                <h2>
-                    Get email updates
-                </h2>
-                <h5>Sign up to get info on all deals and events</h5>
-                <div className="container FooterForm p-0">
-                    <form className="flex flex-col border-2 ml-4 mr-4 bg-slate-50 md:flex-row">
-                    <div className="mb-4" >
-                        <input type="text" placeholder="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"></input>
+            <div className="container lg:flex md:justify-center bg-[#082D46] ">
+                <div className="text-[#FFFF] text-left p-4">
+                    <h2 className="text-3xl font-bold">
+                        Subscribe To Our Newsletter
+                    </h2>
+                    <h5>Sign up to get info on all deals and events</h5>
+                </div>
+                <div className="FooterForm p-2 pb-2 rounded">
+                    <form className="flex flex-col border-2 rounded-md ml-4 mr-4 bg-slate-50 md:flex-row">
+                    <div className="md:box-content border-r-2 border-dotted w-full pl-5 pr-9 pt-1 " >
+                        <input type="text" placeholder=" Name" className="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white  dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"></input>
                     </div>
-                    <div className="mb-4">
-                        <input type="text" placeholder="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"></input>
+                    
+                    <div className="mb-4 md:mb-4 w-full pr-9 pt-1 pl-5">
+                        <input type="text" placeholder="Email" className="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"></input>
                     </div>
-                    <div>
-                        <button className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Subscribe</button>
-                    </div>
+                        <button className="md:auto text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Subscribe</button>
                     </form>
                 </div>
             </div>
