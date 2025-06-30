7da88dff0cf9a15f3057583e7f97390914394d34
drishyaa1 <drishyabhattarai@gmail.com>
2025-06-30 13:09:14 +0545

    testiminial 95% completed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import Carousel from "../components/Layout/Carousels";
 import aboutme from "../assets/aboutme.jpg"
 import CardThings from "../components/Layout/CardThings";
 import CarouselImages from "../components/Layout/CarouselImages";
+import Testimonials from "../components/Layout/Testimonials";
 
 const HomePage=()=>{
     return(
@@ -13,6 +14,10 @@ const HomePage=()=>{
             <div className="mt-96">
                 <CardThings />
 
+            </div>
+
+            <div className="mt-100"> 
+                <Testimonials/>
             </div>
         </div>
          
